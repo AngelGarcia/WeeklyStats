2f8a30cee42a08534d03f0715febfa892b04f6e9
Ángel García Logrosán <aglogrosan@gmail.com>
2025-11-19 17:09:20 +0000

    Try fixing this error: `Runtime Error: Error: Cannot read properties of

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -64,7 +64,7 @@ export default function HistoryPage() {
                                 <div className="flex items-center gap-2">
                                   <Clock className="w-4 h-4" /> 
                                   <span>
-                                    {format(parseISO(meeting.actualStartTime), 'HH:mm')}h - {format(parseISO(meeting.endTime), 'HH:mm')}h
+                                    {format(parseISO(meeting.actualStartTime), 'HH:mm')}h - {meeting.endTime ? format(parseISO(meeting.endTime), 'HH:mm')+'h' : ''}
                                     ({formatTime(totalDuration)})
                                   </span>
                                 </div>
