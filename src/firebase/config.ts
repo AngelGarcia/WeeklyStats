8d71ccd1b481def6124f2a72b1d0a6d5ec9a747a
Ángel García Logrosán <aglogrosan@gmail.com>
2025-11-20 15:08:41 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2737923508-d77d8",
+  "appId": "1:1016870504442:web:e24ec26e44959a10b9c643",
+  "apiKey": "AIzaSyD6aJratQYb-TGgJ6KZqFjf0bf0fYtDEgU",
+  "authDomain": "studio-2737923508-d77d8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1016870504442"
+};
