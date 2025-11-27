adcccf852690ba8c4882c4b72fd7f84a95db1e09
Ángel García Logrosán <aglogrosan@gmail.com>
2025-11-27 15:27:23 +0000

    cuando añado un tema a la agenda y le vuelvo a dar a "añadir" los combos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -109,6 +109,7 @@ export default function MeetingDashboardPage() {
       setNewTopicTitle('');
       setNewTopicDescription('');
       setNewTopicPresenterId(undefined);
+      setNewTopicDuration(5);
     }
   };
 
